834d7e4c36d87ac7dcbfa6722e7d03fd2148f05f
Vojtech Bocek <vbocek@gmail.com>
2015-03-05 23:21:49 +0100

    Add UI for entering pattern for device decryption
    Change-Id: Ia2d3268a96423e9ca3846500c57e674c4f8fa60b
    Signed-off-by: Vojtech Bocek <vbocek@gmail.com>

--- variables.h
+++ variables.h
@@ -147,6 +147,7 @@
 #define TW_IS_ENCRYPTED               "tw_is_encrypted"
 #define TW_IS_DECRYPTED               "tw_is_decrypted"
 #define TW_HAS_CRYPTO                 "tw_has_crypto"
+#define TW_CRYPTO_PWTYPE              "tw_crypto_pwtype"
 #define TW_CRYPTO_PASSWORD            "tw_crypto_password"
 #define TW_DATA_BLK_DEVICE            "tw_data_blk_device"  // Original block device - not decrypted
 #define TW_SDEXT_DISABLE_EXT4         "tw_sdext_disable_ext4"
